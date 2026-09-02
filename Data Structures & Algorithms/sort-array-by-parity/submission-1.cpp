1f0290740210caa5952e5814630edd36fb201d64
Ved-Anand-Khadkikar <130249839+Ved-Anand-Khadkikar@users.noreply.github.com>
2026-09-03 00:00:35 +0530

    Add: sort-array-by-parity - submission-1

---
+++ Data Structures & Algorithms/sort-array-by-parity/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int odd = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] % 2 != 0) {
+                odd = i;
+                break;
+            }
+        }
+        if (odd != -1) {
+            for (int i = odd + 1; i < nums.size(); i++) {
+                if (nums[i] % 2 == 0) {
+                    swap(nums[odd], nums[i]);
+                    odd++;
+                }
+            }
+        }
+        return nums;
+    }
+};
